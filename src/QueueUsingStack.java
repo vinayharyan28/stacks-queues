69c68239f32e1b369b208055224c1807c98f782a
vinayharyan28 <vinayharyan28@gmail.com>
2023-08-12 14:02:58 +0530

    working on leet code queustion

---
+++ src/QueueUsingStack.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class QueueUsingStack {
+    private Stack<Integer> first, second;
+
+    public QueueUsingStack(){
+        first = new Stack<>();
+        second = new Stack<>();
+    }
+
+    public void add(int item){
+        first.push(item);
+    }
+
+    public int remove(){
+        while (!first.isEmpty()){
+            second.push(first.pop());
+        }
+
+        int removed = second.pop();
+
+        while (!second.isEmpty()){
+            first.push(second.pop());
+        }
+
+        return removed;
+    }
+
+    public boolean isEmpty(){
+        return first.isEmpty();
+    }
+
+    // Leet code
+
+}
