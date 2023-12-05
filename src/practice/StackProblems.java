8f5b996540ae749e56b43a61824c59605d6f1f53
vinayharyan28 <vinayharyan28@gmail.com>
2023-12-05 22:08:33 +0530

    testing

--- src/practice/StackProblems.java
+++ src/practice/StackProblems.java
@@ -1,5 +1,6 @@
 package practice;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class StackProblems {
@@ -47,6 +48,27 @@ public class StackProblems {
         System.out.println();
     }
 
+    static void stockSpan(int[] stocks, int[] span){
+        Stack<Integer> stack = new Stack<>();
+        span[0] = 1;
+        stack.push(0);
+
+        for(int i=1; i<stocks.length; i++){
+            int currentPrice = stocks[i];
+            while (!stack.isEmpty() && currentPrice > stocks[stack.peek()]){
+                stack.pop();
+            }
+            if (stack.isEmpty()){
+                span[i] = i+1;
+            }else {
+                int prevHigh = stack.peek();
+                span[i] = i - prevHigh;
+            }
+
+            stack.push(i);
+        }
+    }
+
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
         stack.add(1);
@@ -65,6 +87,10 @@ public class StackProblems {
 //        printStack(reverseStack);
         reverseStack(reverseStack);
         printStack(reverseStack);
+        int[] stocks = {100, 80, 60, 70, 60, 85, 100};
+        int[] span = new int[stocks.length];
+        stockSpan(stocks, span);
+        System.out.println(Arrays.toString(span));
 
 
     }
